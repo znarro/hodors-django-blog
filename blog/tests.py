04a12be47275f26d388381054e40491d16d587f6
Josue Narro <josue.narro.a@gmail.com>
2020-12-18 12:26:05 -0500

    Add CRUD test cases

--- blog/tests.py
+++ blog/tests.py
@@ -29,6 +29,9 @@ class BlogTests(TestCase):
         post = Post(title="A sample title")
         self.assertEqual(str(post), post.title)
 
+    def test_get_absolute_url(self):
+        self.assertEqual(self.post.get_absolute_url(), "/post/1/")
+
     def test_post_content(self):
         """
         Confirm that its content is correct
@@ -43,7 +46,7 @@ class BlogTests(TestCase):
         body text, and uses the correct home.html template
         """
         response = self.client.get(reverse("home"))
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, 200)  # 200: OK
         self.assertContains(response, "Nice body content")
         self.assertTemplateUsed(response, "home.html")
 
@@ -55,6 +58,33 @@ class BlogTests(TestCase):
         response = self.client.get("/post/1/")
         no_response = self.client.get("/post/100000/")
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(no_response.status_code, 404)
+        self.assertEqual(no_response.status_code, 404)  # 404: Not found
         self.assertContains(response, "A good title")
         self.assertTemplateUsed(response, "post_detail.html")
+
+    def test_post_create_view(self):
+        response = self.client.post(
+            reverse("post_new"),
+            {
+                "title": "New title",
+                "body": "New text",
+                "author": self.user,
+            },
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "New title")
+        self.assertContains(response, "New text")
+
+    def test_post_update_view(self):
+        response = self.client.post(
+            reverse("post_edit", args="1"),
+            {
+                "title": "Updated title",
+                "body": "Updated text",
+            },
+        )
+        self.assertEqual(response.status_code, 302)  # 302: Found, redirect
+
+    def test_post_delete_view(self):
+        response = self.client.post(reverse("post_delete", args="1"))
+        self.assertEqual(response.status_code, 302)
